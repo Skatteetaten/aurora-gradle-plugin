23673ccf7e0fa1e78980bbde6f63ec7dec2af2d5
Anders Mikkelsen <Anders.Mikkelsen@skatteetaten.no>
2021-02-08 10:23:20 +0100

    bumped versions

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,17 +2,17 @@ object Versions {
     const val javaSourceCompatibility: String = "11"
     const val groovy: String = "3.0.5"
     const val spock: String = "1.3-groovy-2.5"
-    const val junit5: String = "5.7.0"
+    const val junit5: String = "5.7.1"
     const val cglib: String = "3.3.0"
     const val objenesis: String = "3.1"
     const val auroraSpringBootMvcStarter: String = "1.1.+"
     const val auroraSpringBootWebFluxStarter: String = "1.1.+"
-    const val springCloudContract: String = "3.0.0"
+    const val springCloudContract: String = "3.0.1"
     const val kotlinLogging: String = "2.0.4"
     const val checkstyleConfig: String = "2.2.5"
     const val checkstyleConfigFile: String = "checkstyle/checkstyle-with-metrics.xml"
-    const val kotlin = "1.4.21-2"
-    const val assertk = "0.23"
+    const val kotlin = "1.4.30"
+    const val assertk = "0.23.1"
 }
 
 object PluginVersions {
@@ -23,9 +23,9 @@ object PluginVersions {
     const val gradle_plugin_publish = "0.12.0"
     const val asciidoctor = "3.3.0"
     const val pitest = "1.5.2"
-    const val cloud_contract = "3.0.0"
+    const val cloud_contract = "3.0.1"
     const val dependency_management = "1.0.11.RELEASE"
-    const val sonar = "3.1"
+    const val sonar = "3.1.1"
     const val git_properties = "2.2.4"
     const val latest_versions = "0.2.15"
 }

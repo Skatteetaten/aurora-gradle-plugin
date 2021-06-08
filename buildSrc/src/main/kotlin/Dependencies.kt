9369b48eb00b24b79cca25043c228fdba6d64715
Anders Mikkelsen <Anders.Mikkelsen@skatteetaten.no>
2021-06-08 09:27:19 +0200

    update for 1.2.x starters

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,10 +6,10 @@ object Versions {
     const val jacocoTools: String = "0.8.7"
     const val cglib: String = "3.3.2"
     const val objenesis: String = "3.1"
-    const val auroraSpringBootMvcStarter: String = "1.1.+"
-    const val auroraSpringBootWebFluxStarter: String = "1.1.+"
+    const val auroraSpringBootMvcStarter: String = "1.2.+"
+    const val auroraSpringBootWebFluxStarter: String = "1.2.+"
     const val springCloudContract: String = "3.0.3"
-    const val kotlinLogging: String = "2.0.6"
+    const val kotlinLogging: String = "2.0.8"
     const val checkstyleConfig: String = "2.2.5"
     const val checkstyleConfigFile: String = "checkstyle/checkstyle-with-metrics.xml"
     const val kotlin = "1.5.10"
@@ -18,7 +18,7 @@ object Versions {
 
 object PluginVersions {
     const val gradle_test_logger = "3.0.0"
-    const val ktlint = "10.0.0"
+    const val ktlint = "10.1.0"
     const val spring_boot = "2.5.0"
     const val ben_manes_versions = "0.39.0"
     const val gradle_plugin_publish = "0.15.0"
